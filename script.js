09c9b325cc9e0b2ff5a4f671ba38475737b2f0be
badathala jaisurya <96124846+jaisurya0216@users.noreply.github.com>
2026-02-03 23:20:02 +0530

    script.js

--- script.js
+++ script.js
@@ -57,10 +57,10 @@ const messages = [
     "Are you sure?",
     "Really sure??",
     "Are you positive?",
-    "Pookie please...",
+    "Fondhi fleezzz...",
     "Just think about it!",
-    "If you say no, I will be really sad...",
-    "I will be very sad...",
+    "If you say nyo, I will be really chad...",
+    "I will be very chad...",
     "I will be very very very sad...",
     "Ok fine, I will stop asking...",
     "Just kidding, say yes please! ❤️"
